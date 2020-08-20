9ce4f91c862fae4ce21b58061c4cd56315c40ba1
UM682 <skn.fjc.bbb@gmail.com>
2020-08-20 21:15:41 +0900

    commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center"></div>
+      <v-card>
+        <v-card-title class="headline">motoki web</v-card-title>
+        <v-card-text>
+          <p>webをはじめたよ</p>
+          <p>webをはじめたよ</p>
+          <p>webをはじめたよ</p>
+          <p>webをはじめたよ</p>
+          <p>webをはじめたよ</p>
+          <div class="text-xs-right">
+            <em>
+              <small>もとき</small>
+            </em>
+          </div>
+          <hr class="my-3" />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+};
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center"></div>
+      <v-card>
+        <v-card-title class="headline">ブログ</v-card-title>
+        <v-card-text>
+          <p>すこしずつ更新していく予定。毎日更新していく予定。予定予定予定予定予定予定</p>
+          <div class="text-xs-right">
+            <em>
+              <small>もとき</small>
+            </em>
+          </div>
+          <hr class="my-3" />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+};
+</script>
